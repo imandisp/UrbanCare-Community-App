c6f4cbf645d0cf69104e91bcaf04acb2648793a8
imandisp <imandisp04@gmail.com>
2026-03-13 12:27:55 +0530

    create schemas/complaint.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.routes import auth_routes
 
+from app.models.user import User
+from app.models.location import Location
+from app.models.complaint import Complaint
+from app.models.complaint_image import ComplaintImage
+
 app = FastAPI()
 
 app.include_router(auth_routes.router)
